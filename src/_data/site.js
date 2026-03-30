8df5bae0b23ba0ec8632e6397322066835f883be
jun <jun.drums.1102@gmail.com>
2026-03-30 16:54:10 +1100

    add cso

---
+++ src/_data/site.js
@@ -0,0 +1,18 @@
+module.exports = {
+  url: "https://jun-engineer.github.io",
+  title: "Jun Nammoku | Cloud & Platform Engineer",
+  titleJa: "南木 潤 | クラウド＆プラットフォームエンジニア",
+  description:
+    "Portfolio and tech blog by Jun Nammoku (南木 潤), a cloud engineer based in Sydney specialising in AWS, Azure, and infrastructure automation.",
+  descriptionJa:
+    "南木 潤（Jun Nammoku）のポートフォリオとテックブログ。シドニー在住のクラウドエンジニアとして、AWS・Azure・インフラ自動化に関する知見を発信しています。",
+  author: {
+    name: "Jun Nammoku",
+    nameJa: "南木 潤",
+    jobTitle: "Cloud Engineer",
+    url: "https://jun-engineer.github.io",
+    github: "https://github.com/Jun-engineer",
+    linkedin: "https://www.linkedin.com/in/jun-nammoku/"
+  },
+  image: "/photo/profile.jpg"
+};
